65a59f3fd0deac443a696cbd9b49dfca7730a348
ruan09ju <ju20059499ruan@gmail.com>
2026-04-09 11:48:53 +0800

    0409d

--- web.py
+++ web.py
@@ -39,7 +39,7 @@ def read():
     Result = ""
     db = firestore.client()
     collection_ref = db.collection("靜宜資管2026B")    
-    docs = collection_ref.get()    
+    docs = collection_ref.order_by("lab").get()   
     for doc in docs:         
         Result += str(doc.to_dict()) + "<br>"    
     return Result
